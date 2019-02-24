fff25bb62af8909369c81bf9eaa520066f222903
E Pereira <pereriaerika@outlook.com>
2019-02-24 00:58:30 +0000

    Inventory & get key

--- sexting/actions.py
+++ sexting/actions.py
@@ -6,15 +6,22 @@ from sexting import state
 def do_action(action, words):
     if action == 'look':
         print(f" Location: {state.player.location.name} ")
+        #room contains
         for item in state.player.location.contains:
             print(f"Room contains: ",item)
 
     elif action == 'examine' and len(words) == 1:
         item = state.player.location.find_object(words[0])
+        if not item:
+            print("Item does not exist")
+            return
         print(f"You can do these actions: ",item.get_actions())
 
     elif action and len(words)== 1:
         item = state.player.location.find_object(words[0])
+        if not item:
+            print("Item does not exist")
+            return
         action_valid = action in item.get_actions()
 
 
@@ -23,7 +30,7 @@ def do_action(action, words):
         if action_valid and method_exists:
             getattr(item,action)()
         else:
-            print("arse")
+            print(item.bad_action(action))
 
     else:
         print("Boobs")
--- sexting/state.py
+++ sexting/state.py
@@ -4,10 +4,13 @@ class Player(object):
         self.is_alive = True
         self.is_trapped = True
         self.location = location
+        self.inventory = Inventory()
 
     def dead(self):
         self.is_alive = False
         print('You died of dysentery')
+
+
 
 
 class Location(object):
@@ -23,10 +26,6 @@ class Location(object):
             if item_name.lower() == str(item).lower():
                 return item
         return None
-
-
-
-
 
 
 class Door(object):
@@ -67,12 +66,76 @@ class Door(object):
         return "You can't do this."
 
 
+class LockedDoor(Door):
+    is_locked = True
+
+    def __init__(self,key):
+        self.key = key
+
+    def open(self):
+        if self.is_locked:
+            print('the door is locked')
+
+        else:
+            super().open()
+
+    def unlock(self):
+
+        if self.key in player.inventory:
+            self.is_locked = False
+            print("You unlocked the door")
+
+        else:
+            print("You don't have a key")
+
+
+    def get_actions(self):
+        return super().get_actions() + ['unlock']
+
+
+
+
+class Inventory(object):
+    def __init__(self):
+        self.inventory_items = []
+
+    def __contains__(self, item):
+        return item in self.inventory_items
+
+    def add_inventory_item (self, inventory_item):
+        self.inventory_items.append(inventory_item)
+
+
+
+
+class InventoryItem(object):
+    actions = ['get']
+
+    def __str__(self):
+        return self.name
+
+    def __init__(self,name):
+        self.name = name
+
+    def get(self):
+        player.inventory.add_inventory_item(self)
+
+    def get_actions(self):
+        return self.actions
+
+    def bad_action(self,action):
+        return "You can't do this."
 
 
 lounge = Location(name='lounge')
 
 player = Player(location=lounge)
 
-lounge_door = Door()
-lounge.add_item(lounge_door)
 
+
+lounge_key = InventoryItem('key')
+
+
+lounge_door = LockedDoor(lounge_key)
+lounge.add_item(lounge_door)
+lounge.add_item(lounge_key)
